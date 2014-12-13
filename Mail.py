72b0d05c2b916595ab3785dac9a2feed6a249588
Kacper20 <kacper.harasim@gmail.com>
2014-12-13 21:37:42 +0100

    logowanie - podstawowa funkcjonalnosc

--- Mail.py
+++ Mail.py
@@ -1,16 +1,30 @@
-from flask import Flask
+from flask import Flask, session, request, redirect, url_for, render_template
+from Login import valid_login
+import os
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] and request.form['password']:
+            username = request.form['username']
+            password = request.form['password']
+            if valid_login(username, password):
+                session['username'] = login
+                return redirect(url_for('index'))
+            else:
+                return render_template('login.html')
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
 
 
-def costam():
-    return 'dupa'
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
