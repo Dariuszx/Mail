c30c01843c4a82c9c1be7c661b6ed47887e5b0af
Kacper20 <kacper.harasim@gmail.com>
2014-12-13 21:09:41 +0100

    dodane podstawowe testy

--- Login.py
+++ Login.py
@@ -1,2 +1,19 @@
 __author__ = 'dariusz'
 
+import requests, json
+
+
+# Function to validate
+#
+
+def valid_login(username, password):
+    base_path = 'http://private-anon-09c2921dd-bach.apiary-proxy.com/staff/~chaberb/apps/mail/login/'
+    val = password
+    path = base_path + username
+    headers = {"Content-Type": "text/plain"}
+    r = requests.post(path, data=val, headers=headers)
+    print r.status_code
+    if r.status_code == requests.codes.ok:
+        return True
+    else:
+        return False
--- Mail.py
+++ Mail.py
@@ -1,17 +1,8 @@
 from flask import Flask
-import requests
 app = Flask(__name__)
 
 
 
-@app.route('/login/<username>')
-def login(username):
-    base_path = 'http://private-anon-09c2921dd-bach.apiary-proxy.com/staff/~chaberb/apps/mail/login/'
-    val = username
-    path = base_path + username
-    headers = {"Content-Type": "text/plain"}
-    r = requests.post(path, data=val, headers=headers)
-    return r.content
 
 
 @app.route('/')
